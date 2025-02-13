f1157277d3d979441aca9bc8be8f7f3e8a498b7e
Wes Feltz <wesfeltz@gmail.com>
2025-02-12 22:40:11 -0500

    initial

---
+++ src/main/java/com/wesfeltz/Playwright/Playwright.java
@@ -0,0 +1,10 @@
+package com.wesfeltz.Playwright;
+
+/**
+ * Hello world!
+ */
+public class Playwright {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
